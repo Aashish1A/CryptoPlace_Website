54022b167b5fe5d8c1596be126f1359543b0efeb
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-21 17:53:53 +0530

    Created linechart and coin components

--- src/Pages/Coin/Coin.jsx
+++ src/Pages/Coin/Coin.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { CoinContext } from '../../Context/CoinContext';
+import LineChart from '../../Components/LineChart/LineChart';
 
 const Coin = () => {
 
@@ -29,7 +30,7 @@ const Coin = () => {
       headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-kQ1ZwQmfUf8QtzBUKhxS8TEi'}
     };
     
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency}&days=10`, options)
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency}&days=10&interval=daily`, options)
       .then(res => res.json())
       .then(res => setHistoricalData(res))
       .catch(err => console.error(err));
@@ -37,18 +38,42 @@ const Coin = () => {
 
   useEffect(()=> {
     fetchCoinData();
+    fetchHistoricalData();
   },[currency])
 
-  useEffect(()=> {
-    fetchHistoricalData();
-  }, [])
 
   if(coinData, historicalData){
     return (
-      <div className=''>
-          <div className="">
-            <img src={coinData.image.large} alt="" />
-            <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+      <div className='py-0 px-20px'>
+          <div className="flex flex-col items-center gap-5 my-25 mx-auto mb-12">
+            <img src={coinData.image.large} alt="" className='max-w-25' />
+            <p className='text-11 font-medium'><b className='text-11 font-medium'>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+          </div>
+          <div className="max-w-[600px] h-62 m-auto">
+            <LineChart historicalData={historicalData} />
+          </div>
+
+          <div className="max-w-[600px] my-12 mx-auto flex flex-col coin-info">
+            <ul>
+              <li>Crypto Market Rank</li>
+              <ul>{coinData.market_cap_rank}</ul>
+            </ul>
+            <ul>
+              <li>Current Price</li>
+              <ul>{currency.symbol} {coinData.market_data.current_price[currency.name].toLocaleString()}</ul>
+            </ul>
+            <ul>
+              <li>Market Cap</li>
+              <ul>{currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</ul>
+            </ul>
+            <ul>
+              <li>24 Hour high</li>
+              <ul>{currency.symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</ul>
+            </ul>
+            <ul>
+              <li>24 Hour Low</li>
+              <ul>{currency.symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</ul>
+            </ul>
           </div>
       </div>
     )
